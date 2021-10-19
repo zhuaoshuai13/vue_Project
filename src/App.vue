a91d82ef6da0697ae8bb5816716ee482b585a9a0
zhuao <531024850@qq.com>
2021-10-19 23:19:08 +0800

    完成tab-bar的组件搭建

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
-    app
     <router-view />
-    <tab-bar/>
+    <tab-bar :tabbar="tabbar"/>
   </div>
 </template>
 <script>
@@ -11,8 +10,39 @@ export default {
   components: {
     TabBar,
   },
+  data() {
+    return {
+      tabbar: [{
+        title: '首页',
+        icon: '&#xe601;',
+        path: '/home',
+      }, {
+        title: '消息',
+        icon: '&#xe610;',
+        path: '/category',
+      }, {
+        title: '购物车',
+        icon: '&#xe603;',
+        path: '/cart',
+      }, {
+        title: '我的',
+        icon: '&#xe604;',
+        path: '/mine',
+      }],
+    }
+  },
 }
 </script>
 <style lang="less">
-
+  body{
+    height: 667px;
+  }
+  #app{
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    div{
+      flex: 1;
+    }
+  }
 </style>
--- src/components/tab-bar/index.vue
+++ src/components/tab-bar/index.vue
@@ -1,8 +1,14 @@
 <template>
       <ul class="tab-bar">
-        <li v-for="n in 4" :key="n">
-          <i>图标</i>
-          <span>文本</span>
+        <li
+          is="router-link"
+          tag="li"
+          v-for="item of tabbar"
+           :key="item.path"
+          :to="item.path"
+        >
+          <i class="iconfont" v-html="item.icon"></i>
+          <span>{{item.title}}</span>
         </li>
       </ul>
 </template>
@@ -10,11 +16,45 @@
 <script>
 export default {
   name: 'TabBar',
+  props: {
+    tabbar: {
+      value: 'tabbar',
+    },
+  },
 }
 </script>
 
 <style lang="less" scoped>
+  @font-face {
+  font-family: 'iconfont';  /* Project id 2878959 */
+  src: url('//at.alicdn.com/t/font_2878959_94wymr0tvvd.woff2?t=1634643754669') format('woff2'),
+       url('//at.alicdn.com/t/font_2878959_94wymr0tvvd.woff?t=1634643754669') format('woff'),
+       url('//at.alicdn.com/t/font_2878959_94wymr0tvvd.ttf?t=1634643754669') format('truetype');
+}
+  .iconfont{
+    font-family:"iconfont" !important;
+    font-size:24px;font-style:normal;
+    -webkit-font-smoothing: antialiased;
+    -webkit-text-stroke-width: 0.2px;
+    -moz-osx-font-smoothing: grayscale;
+    }
   ul{
     display: flex;
+    justify-content: space-around;
+    align-items: center;
+    height: 78px;
+    border-top: 1px solid #ccc;
+    i{
+      display: block;
+      width: 40px;
+      text-align: center;
+    }
+    span{
+      display: block;
+      text-align: center;
+    }
+    .router-link-exact-active, .router-link-active {
+    color: #d81e06;
+  }
   }
 </style>
